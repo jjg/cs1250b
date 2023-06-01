f3a912380f1dbdd47949d66c60448673aec1a20c
Jason J. Gullickson <mr@jasongullickson.com>
2023-06-01 09:59:36 -0500

    Try basic ISR.

---
+++ pages/posts/[id].js
@@ -0,0 +1,30 @@
+export default function Page({ posts }) {
+  return posts[0].title;
+}
+ 
+export async function getStaticProps(context) {
+  const output = context.draftMode
+    ? 'draft mode'
+    : 'normal mode';
+  return {
+    props: {
+      posts: [
+        {
+          title: output,
+        },
+      ],
+    },
+    revalidate: 10, // In seconds
+  };
+}
+ 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { id: '1' },
+      },
+    ],
+    fallback: 'blocking',
+  };
+}
